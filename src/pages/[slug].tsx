c9d632aa1dd1fa8e8a589351ae301293a80d2748
Slimani Imed Eddine Abderrahmane <imeds2000@gmail.com>
2023-09-03 20:48:14 +0100

    seperate ssg helper, full post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,11 @@
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from '@trpc/react-query/server';
-import { appRouter } from '~/server/api/root';
-import { prisma } from "~/server/db"
-import superjson from 'superjson';
 import { type GetStaticProps, type NextPage } from "next";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image"
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -64,15 +61,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 export default ProfilePage
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: {
-      prisma,
-      userId: null
-    },
-    transformer: superjson,
-  });
-
+  const ssg = generateSSGHelper()
   const slug = context.params?.slug
 
   if (typeof slug !== "string") throw new Error("no slug")
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,16 +1,50 @@
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { type GetStaticProps, type NextPage } from "next";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
-export default function SinglePostPage() {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id
+  })
+
+  if (!data) return <div>!404</div>
 
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - ${data.author.username}`}</title>
       </Head>
-      <main className="flex justify-center h-screen">
-        <div>Post view</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
 }
 
+export default SinglePostPage
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper()
+  const id = context.params?.id
+
+  if (typeof id !== "string") throw new Error("no id")
+
+  await ssg.posts.getById.prefetch({ id })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id
+    }
+  }
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking"
+  }
+}
